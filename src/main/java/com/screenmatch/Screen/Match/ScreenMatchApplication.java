649c916a39b7a9b1612efed29f3fcdcfc985032a
André, Desenvolvedor de Software <felipecarvalhoacdm@gmail.com>
2025-08-04 06:24:11 -0300

    Correção de funcionalidade

--- src/main/java/com/screenmatch/Screen/Match/ScreenMatchApplication.java
+++ src/main/java/com/screenmatch/Screen/Match/ScreenMatchApplication.java
@@ -17,11 +17,11 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String...args) throws Exception {
 		var apiFilmes = new ConsumoApi(); // Inferência de tipo (primitivo ou contruído)
-		var json = apiFilmes.obterDados("http://www.omdbapi.com/?t=Star+Wars%3A+Episode+IV+-+A+New+Hope&apikey=6c5f82fc");
+		var json = apiFilmes.obterDados("http://www.omdbapi.com/?t=The+Avengers&apikey=6c5f82fc");
 		// Conversor
 		ConverteDados desserializacao = new ConverteDados();
 		// Classe de mapeamento recebe o valor do conversor com seus parametros
 		DadosSerie dadosSerie = desserializacao.obterDados(json, DadosSerie.class);
-		System.out.printf("Titulo do filme: %s e seu lancamento foi em ", dadosSerie.title(), dadosSerie.year());
+		System.out.printf("Titulo do filme: %s e seu lancamento foi em %d", dadosSerie.title(), dadosSerie.year());
 	}
 }
